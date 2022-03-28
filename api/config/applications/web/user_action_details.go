ea7851d8b49fdb01a4566fc188853307dedc84aa
come.to.the.dark.side.cookie@gmail.com <reinhard.pilz@dynatrace.com>
2022-03-28 10:57:15 +0200

    property caseSensitive for UserActionDetails must not be optional

--- api/config/applications/web/user_action_details.go
+++ api/config/applications/web/user_action_details.go
@@ -4,11 +4,11 @@ import "github.com/dtcookie/hcl"
 
 // UserActionDetails Configuration of a user action-based conversion goal
 type UserActionDetails struct {
-	Value         *string      `json:"value,omitempty"`         // The value to be matched to hit the conversion goal
-	CaseSensitive bool         `json:"caseSensitive,omitempty"` // The match is case-sensitive (`true`) or (`false`)
-	MatchType     *MatchType   `json:"matchType,omitempty"`     // The operator of the match. Possible values are `Begins`, `Contains` and `Ends`.
-	MatchEntity   *MatchEntity `json:"matchEntity,omitempty"`   // The type of the entity to which the rule applies. Possible values are `ActionName`, `CssSelector`, `JavaScriptVariable`, `MetaTag`, `PagePath`, `PageTitle`, `PageUrl`, `UrlAnchor` and `XhrUrl`.
-	ActionType    *ActionType  `json:"actionType,omitempty"`    // Type of the action to which the rule applies. Possible values are `Custom`, `Load` and `Xhr`.
+	Value         *string      `json:"value,omitempty"`       // The value to be matched to hit the conversion goal
+	CaseSensitive bool         `json:"caseSensitive"`         // The match is case-sensitive (`true`) or (`false`)
+	MatchType     *MatchType   `json:"matchType,omitempty"`   // The operator of the match. Possible values are `Begins`, `Contains` and `Ends`.
+	MatchEntity   *MatchEntity `json:"matchEntity,omitempty"` // The type of the entity to which the rule applies. Possible values are `ActionName`, `CssSelector`, `JavaScriptVariable`, `MetaTag`, `PagePath`, `PageTitle`, `PageUrl`, `UrlAnchor` and `XhrUrl`.
+	ActionType    *ActionType  `json:"actionType,omitempty"`  // Type of the action to which the rule applies. Possible values are `Custom`, `Load` and `Xhr`.
 }
 
 func (me *UserActionDetails) Schema() map[string]*hcl.Schema {
