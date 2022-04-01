036c57b560de9e3bc4571561d0c14a7e4aef562f
come.to.the.dark.side.cookie@gmail.com <reinhard.pilz@dynatrace.com>
2022-04-01 11:41:43 +0200

    special handling for resource_time_capture_type

--- api/config/applications/web/resource_time_settings.go
+++ api/config/applications/web/resource_time_settings.go
@@ -4,11 +4,11 @@ import "github.com/dtcookie/hcl"
 
 // ResourceTimingSettings configures resource timings capture
 type ResourceTimingSettings struct {
-	W3CResourceTimings                        bool                      `json:"w3cResourceTimings"`                        // W3C resource timings for third party/CDN enabled/disabled
-	NonW3CResourceTimings                     bool                      `json:"nonW3cResourceTimings"`                     // Timing for JavaScript files and images on non-W3C supported browsers enabled/disabled
-	NonW3CResourceTimingsInstrumentationDelay int32                     `json:"nonW3cResourceTimingsInstrumentationDelay"` // Instrumentation delay for monitoring resource and image resource impact in browsers that don't offer W3C resource timings. \n\nValid values range from 0 to 9999.\n\nOnly effective if **nonW3cResourceTimings** is enabled
-	ResourceTimingCaptureType                 ResourceTimingCaptureType `json:"resourceTimingCaptureType"`                 // Defines how detailed resource timings are captured.\n\nOnly effective if **w3cResourceTimings** or **nonW3cResourceTimings** is enabled. Possible values are `CAPTURE_ALL_SUMMARIES`, `CAPTURE_FULL_DETAILS` and `CAPTURE_LIMITED_SUMMARIES`
-	ResourceTimingsDomainLimit                int32                     `json:"resourceTimingsDomainLimit"`                // Limits the number of domains for which W3C resource timings are captured.\n\nOnly effective if **resourceTimingCaptureType** is `CAPTURE_LIMITED_SUMMARIES`. Valid values range from 0 to 50.
+	W3CResourceTimings                        bool                       `json:"w3cResourceTimings"`                        // W3C resource timings for third party/CDN enabled/disabled
+	NonW3CResourceTimings                     bool                       `json:"nonW3cResourceTimings"`                     // Timing for JavaScript files and images on non-W3C supported browsers enabled/disabled
+	NonW3CResourceTimingsInstrumentationDelay int32                      `json:"nonW3cResourceTimingsInstrumentationDelay"` // Instrumentation delay for monitoring resource and image resource impact in browsers that don't offer W3C resource timings. \n\nValid values range from 0 to 9999.\n\nOnly effective if **nonW3cResourceTimings** is enabled
+	ResourceTimingCaptureType                 *ResourceTimingCaptureType `json:"resourceTimingCaptureType"`                 // Defines how detailed resource timings are captured.\n\nOnly effective if **w3cResourceTimings** or **nonW3cResourceTimings** is enabled. Possible values are `CAPTURE_ALL_SUMMARIES`, `CAPTURE_FULL_DETAILS` and `CAPTURE_LIMITED_SUMMARIES`
+	ResourceTimingsDomainLimit                *int32                     `json:"resourceTimingsDomainLimit"`                // Limits the number of domains for which W3C resource timings are captured.\n\nOnly effective if **resourceTimingCaptureType** is `CAPTURE_LIMITED_SUMMARIES`. Valid values range from 0 to 50.
 }
 
 func (me *ResourceTimingSettings) Schema() map[string]*hcl.Schema {
@@ -42,21 +42,32 @@ func (me *ResourceTimingSettings) Schema() map[string]*hcl.Schema {
 }
 
 func (me *ResourceTimingSettings) MarshalHCL() (map[string]interface{}, error) {
-	return hcl.Properties{}.EncodeAll(map[string]interface{}{
+	res, err := hcl.Properties{}.EncodeAll(map[string]interface{}{
 		"w3c_resource_timings":          me.W3CResourceTimings,
 		"non_w3c_resource_timings":      me.NonW3CResourceTimings,
 		"instrumentation_delay":         me.NonW3CResourceTimingsInstrumentationDelay,
 		"resource_timing_capture_type":  me.ResourceTimingCaptureType,
 		"resource_timings_domain_limit": me.ResourceTimingsDomainLimit,
 	})
+	if err != nil {
+		return nil, err
+	}
+	if me.ResourceTimingCaptureType != nil && len(string(*me.ResourceTimingCaptureType)) == 0 {
+		delete(res, "resource_timing_capture_type")
+	}
+	return res, nil
 }
 
 func (me *ResourceTimingSettings) UnmarshalHCL(decoder hcl.Decoder) error {
-	return decoder.DecodeAll(map[string]interface{}{
+	err := decoder.DecodeAll(map[string]interface{}{
 		"w3c_resource_timings":          &me.W3CResourceTimings,
 		"non_w3c_resource_timings":      &me.NonW3CResourceTimings,
 		"instrumentation_delay":         &me.NonW3CResourceTimingsInstrumentationDelay,
 		"resource_timing_capture_type":  &me.ResourceTimingCaptureType,
 		"resource_timings_domain_limit": &me.ResourceTimingsDomainLimit,
 	})
+	if err != nil {
+		return err
+	}
+	return nil
 }
