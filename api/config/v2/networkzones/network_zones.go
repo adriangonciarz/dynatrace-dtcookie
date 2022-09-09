d675f4ac14993f71d26af87c6615f99d5aa65b1a
Kodai Ishikawa <kodai.msu@gmail.com>
2022-09-09 08:46:08 -0400

    introduced network zones

---
+++ api/config/v2/networkzones/network_zones.go
@@ -0,0 +1,33 @@
+package networkzones
+
+import (
+	"github.com/dtcookie/hcl"
+)
+
+type NetworkZones struct {
+	Enabled bool `json:"enabled"` // Network Zones are enabled (`true`) or disabled (`false`)
+}
+
+func (me *NetworkZones) Schema() map[string]*hcl.Schema {
+	return map[string]*hcl.Schema{
+		"enabled": {
+			Type:        hcl.TypeBool,
+			Optional:    true,
+			Description: "Network Zones are enabled (`true`) or disabled (`false`)",
+		},
+	}
+}
+
+func (me *NetworkZones) MarshalHCL() (map[string]interface{}, error) {
+	properties := hcl.Properties{}
+
+	return properties.EncodeAll(map[string]interface{}{
+		"enabled": me.Enabled,
+	})
+}
+
+func (me *NetworkZones) UnmarshalHCL(decoder hcl.Decoder) error {
+	return decoder.DecodeAll(map[string]interface{}{
+		"enabled": &me.Enabled,
+	})
+}
