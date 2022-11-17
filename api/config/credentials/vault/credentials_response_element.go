1b42d7876a3f6e4570b88c8e174d24eb81b4f498
Reinhard Pilz <reinhard.pilz@dynatrace.com>
2022-11-17 14:41:59 +0100

    Introduced Scope to CredentialsResponseElement

--- api/config/credentials/vault/credentials_response_element.go
+++ api/config/credentials/vault/credentials_response_element.go
@@ -4,15 +4,15 @@ import "github.com/dtcookie/gojson"
 
 // CredentialsResponseElement Metadata of the credentials set.
 type CredentialsResponseElement struct {
+	Name            string                         `json:"name"`            // The name of the credentials set.
+	ID              *string                        `json:"id,omitempty"`    // The ID of the credentials set.
+	Description     string                         `json:"description"`     // A short description of the credentials set.
+	Owner           string                         `json:"owner"`           // The owner of the credential.
 	OwnerAccessOnly bool                           `json:"ownerAccessOnly"` // Flag indicating that this credential is visible only to the owner.
 	Type            CredentialsResponseElementType `json:"type"`            // The type of the credentials set.
-	Description     string                         `json:"description"`     // A short description of the credentials set.
-	ID              *string                        `json:"id,omitempty"`    // The ID of the credentials set.
-	Name            string                         `json:"name"`            // The name of the credentials set.
-	Owner           string                         `json:"owner"`           // The owner of the credential.
+	Scope           Scope                          `json:"scope,omitempty"` // The scope of the credentials set
 }
 
-// UnmarshalJSON provides custom JSON deserialization
 func (cre *CredentialsResponseElement) UnmarshalJSON(data []byte) error {
 	return gojson.Unmarshal(data, cre)
 }
@@ -27,13 +27,15 @@ type CredentialsResponseElementType string
 
 // CredentialsResponseElementTypes offers the known enum values
 var CredentialsResponseElementTypes = struct {
-	Certificate      CredentialsResponseElementType
-	Token            CredentialsResponseElementType
-	Unknown          CredentialsResponseElementType
-	UsernamePassword CredentialsResponseElementType
+	Certificate       CredentialsResponseElementType
+	PublicCertificate CredentialsResponseElementType
+	Token             CredentialsResponseElementType
+	Unknown           CredentialsResponseElementType
+	UsernamePassword  CredentialsResponseElementType
 }{
-	"CERTIFICATE",
-	"TOKEN",
-	"UNKNOWN",
-	"USERNAME_PASSWORD",
+	CredentialsResponseElementType("CERTIFICATE"),
+	CredentialsResponseElementType("PUBLIC_CERTIFICATE"),
+	CredentialsResponseElementType("TOKEN"),
+	CredentialsResponseElementType("UNKNOWN"),
+	CredentialsResponseElementType("USERNAME_PASSWORD"),
 }
